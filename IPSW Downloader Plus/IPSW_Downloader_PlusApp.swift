63d2ca45f00235dc10aed1732c616d737a3fe317
Alessio Cosi <cosi.alessio@icloud.com>
2026-03-15 22:04:28 +0100

    Initial Commit

---
+++ IPSW Downloader Plus/IPSW_Downloader_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPSW_Downloader_PlusApp.swift
+//  IPSW Downloader Plus
+//
+//  Created by Alessio Cosi on 15/03/26.
+//
+
+import SwiftUI
+
+@main
+struct IPSW_Downloader_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
